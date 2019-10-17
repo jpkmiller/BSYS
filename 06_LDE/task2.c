6bc9904ed5343cb6c5b051c3cc7c0c9be65b8293
jpkmiller <j.mueller5021@gmail.com>
2019-10-17 16:15:24 +0200

    fixed any problems. Adding later features to measure context switch using a pipe

--- 06_LDE/task2.c
+++ 06_LDE/task2.c
@@ -1,10 +1,18 @@
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-	int fd[2];
-	if ((pid_t child = fork()) < 0) {
+int main(void) {
+	
+	int fd[2], child;
+	pipe(fd);
+	if ((child = fork()) < 0) {
 		perror("fork");
+		exit(1);
 	} else if (child) {
-		
+		printf("I am parent\n");
+	} else {
+		printf("I am child\n");
 	}
 	return 0;	
 }
