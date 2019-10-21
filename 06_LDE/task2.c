88ce94358f7675cafe4aae19048cb2fa95f74b6e
jpkmiller <j.mueller5021@gmail.com>
2019-10-22 01:48:53 +0200

    reviewed code and fixed some problems not switching back and forth between parent and child.fml

--- 06_LDE/task2.c
+++ 06_LDE/task2.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,44 +9,58 @@
 int main(int argc, char *argv[]) {
 
 	int it = atoi(argv[argc - 1]);
-
-	int fd1[2],fd2[2], child;
-	pipe(fd1);
-	pipe(fd2);
+	
+	int PtoC[2],CtoP[2], child;
+	pipe(PtoC);
+	pipe(CtoP);
 	long int time1[it], time2[it], t1, t2;
 	int i = 0;
+
+	cpu_set_t set;
+	CPU_ZERO(&set);
 
 	struct timespec begin1C, end1C, begin2C, end2C;
 
 	if ((child = fork()) < 0) {
 		perror("fork");
 		exit(1);
-	} else if (child) {
-		close(fd1[1]); //close output of first pipe
-		close(fd2[0]); //close input of second pipe
+	} else if (child == 0) {
+		close(PtoC[1]);
 
-		clock_gettime(CLOCK_REALTIME, &end1C);
-		t1 = (((int) end2C.tv_sec - (int) begin2C.tv_sec) * 1000000000) + (int) end2C.tv_nsec - (int) begin2C.tv_nsec;
-		write(fd1[1], &t1, sizeof(int));
+		CPU_SET(0, &set);
+		sched_setaffinity(getpid(), sizeof(set), &set);
+		while(i < it) {
+			clock_gettime(CLOCK_REALTIME, &end1C);
+			t1 = (( end1C.tv_sec - begin1C.tv_sec) * 1000000000) + end1C.tv_nsec - begin1C.tv_nsec;
+			printf("coming from parent w/ cs time: %ld\n", t1);
+			write(CtoP[1], &t1, sizeof(long int));
+			
+			clock_gettime(CLOCK_REALTIME, &begin2C);
+			read(PtoC[0], &time2[++i] , sizeof(long int)); //context switch to paren
 
-		printf("reading from parent %d\n", i);
-		clock_gettime(CLOCK_REALTIME, &begin2C);
-		read(fd2[0], &time2[i++] , sizeof(int)); 
-
-	} else {
-		while (i < it) {
-			close(fd2[1]); //close input of first pipe
-			close(fd1[0]); //close output of second pipe
-
-			printf("reading from child %d %d\n", i, it);
-			clock_gettime(CLOCK_REALTIME, &begin1C);
-			read(fd1[0], &time1[i++], sizeof(int)); //context switch to child process
-
-			//return from child
-			clock_gettime(CLOCK_REALTIME, &end2C);
-			t2 = (((int) end1C.tv_sec - (int) begin1C.tv_sec) * 1000000000) + (int) end1C.tv_nsec - (int) begin1C.tv_nsec;
-			write(fd2[1], &t2, sizeof(int));
 		}
+		printf("pipe finished\n");
+		exit(0);
+	} else {
+
+		CPU_SET(0, &set);
+		sched_setaffinity(getpid(), sizeof(set), &set);
+
+		while (i < it) {
+
+
+			printf("switch to child\n");
+			clock_gettime(CLOCK_REALTIME, &begin1C);
+			read(CtoP[0], &time1[++i], sizeof(long int)); //context switch to child process
+
+			clock_gettime(CLOCK_REALTIME, &end2C);
+			t2 = ((end2C.tv_sec - begin2C.tv_sec) * 1000000000) + end2C.tv_nsec - begin2C.tv_nsec;
+			printf("comming back from child w/ cs time: %ld\n", t2);
+			close(PtoC[0]);
+			write(PtoC[1], &t2, sizeof(long int));
+		}
+		printf("waiting for child\n");
 	}
-	return 0;	
+	//kill(child, 0);
+	return 0;
 }
