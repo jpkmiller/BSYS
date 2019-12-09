c8d02b2a03580cfe43d7e32b237b4b76cb006ff2
jpkmiller <j.mueller5021@gmail.com>
2019-12-09 02:25:00 +0100

    compiled all files

--- 27_API/main-race.c
+++ 27_API/main-race.c
@@ -3,16 +3,23 @@
 #include "mythreads.h"
 
 int balance = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    pthread_mutex_t *argLock = (pthread_mutex_t *) arg;
+    Pthread_mutex_lock(argLock);
+    balance++; // unprotected access
+    printf("balance updated\n");
+    //Pthread_mutex_unlock(argLock);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
-    Pthread_create(&p, NULL, worker, NULL);
+    Pthread_create(&p, NULL, worker, &lock);
+    Pthread_mutex_lock(&lock);
     balance++; // unprotected access
+    Pthread_mutex_unlock(&lock);
     Pthread_join(p, NULL);
     return 0;
 }
